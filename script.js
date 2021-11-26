6fada322f32e8540ceec02a43702e24a8a46bf27
Yamila Fuentes <yamilan.fuentes@gmail.com>
2021-11-26 14:32:08 -0300

    new commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const navToggle = document.querySelector(".navbar_toggle")
+const links = document.querySelector(".main_nav")
+
+navToggle.addEventListener('click', function(){
+  links.classList.toggle("show_nav")
+})
+
+
+
+
+
