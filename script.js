89da74bd535fe43a35a2a9316fc68230e1dc7401
Yamila Fuentes <yamilan.fuentes@gmail.com>
2021-12-17 16:19:16 -0300

    new commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-
+/* 
 const navToggle = document.querySelector(".navbar_toggle")
 const links = document.querySelector(".main_nav")
 
 navToggle.addEventListener('click', function(){
   links.classList.toggle("show_nav")
-})
+}) */
 
 
 
@@ -13,59 +13,54 @@ const photos = [
     {
       "name": "La mona lisa",
       "price": 15000,
-      "Measures": "150x750",
+      "measures": "150x750",
       "path": "1.jpeg"
     },
     {
       "name": "La noche estrellada",
       "price": 16000,
-      "Measures": "150x750",
+      "measures": "150x750",
       "path": "2.jpeg"
     },
     {
       "name": "El jardin de las delicias",
       "priceo": 17000,
-      "Measures": "150x750",
+      "measures": "150x750",
       "path": "3.jpeg"
     },
     {
       "name": "Muchacha viendo por la ventana",
       "price": 18000,
-      "Measures": "150x750",
+      "measures": "150x750",
       "path": "4.jpeg"
     },
     {
       "name": "El grito",
       "price": 19000,
-      "Measures": "150x750",
+      "measures": "150x750",
       "path": "5.jpeg"
     }
   ]
-  
-stringVacio = ""
+
+console.log("Hi")
+const element = document.querySelector("#top-header")
+element.innerHTML = "Hello world"
+console.log(element)
 
 
+const stringVacio = ""
 
+console.log(photos)
 
-for (i = 0; i<photos.length; i++) {
-  stringVacio = ""
-  saveName = i.name
-  savePrice = i.price
-  saveMeasures = i.Measures
-  savePath = i.path
-
-  <div class="box-image">
-      <img src="images/savePath" alt="saveName" class="image">
-      <h2 class="title-image">Imagen 1</h2>
-  </div>
-
+for (let photo in photos) {
+  Object.keys(photo)
 
 } 
 
 
 
   
-document.getElementById("top-header").innerHTML = stringVacio
+/* document.getElementById("top-header").innerHTML = stringVacio */
 
 
 
